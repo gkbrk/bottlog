cb2d4548d26e6e8acee7b182f6618b4677ff5660
Gökberk <webdosusb@gmail.com>
2015-01-04 13:16:58 +0200

    Added main file

---
+++ bottlog.py
@@ -0,0 +1,38 @@
+import bottle
+import time
+
+def apache_logger(callback):
+    """Logs in the combined log format"""
+    def wrapper(*args, **kwargs):
+        log_data = []
+        log_data.append(bottle.request.remote_addr)
+        log_data.append("-")
+        log_data.append("-")
+        log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
+        log_data.append('"%s %s %s"' % (bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
+        log_data.append(bottle.response.status_code)
+        log_data.append(bottle.response.content_length)
+        log_data.append(bottle.request.get_header("Referer", "-"))
+        log_data.append(bottle.request.get_header("User-Agent", "-"))
+
+        with open("server.log", "a+") as log_file:
+            log_file.writeline(" ".join(log_data))
+
+    return wrapper
+
+def common_logger(callback):
+    """Logs in the common log format"""
+    def wrapper(*args, **kwargs):
+        log_data = []
+        log_data.append(bottle.request.remote_addr)
+        log_data.append("-")
+        log_data.append("-")
+        log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
+        log_data.append('"%s %s %s"' % (bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
+        log_data.append(bottle.response.status_code)
+        log_data.append(bottle.response.content_length)
+
+        with open("server.log", "a+") as log_file:
+            log_file.writeline(" ".join(log_data))
+
+    return wrapper
