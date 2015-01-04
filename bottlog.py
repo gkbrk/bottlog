95430534e5733d3c32c1d29684bdd5b53e585e5a
Gökberk <webdosusb@gmail.com>
2015-01-04 14:11:33 +0200

    Fixed content-length

--- bottlog.py
+++ bottlog.py
@@ -4,6 +4,8 @@ import time
 def apache_logger(callback):
     """Logs in the combined log format"""
     def wrapper(*args, **kwargs):
+        body = callback(*args, **kwargs)
+
         log_data = []
         log_data.append(bottle.request.remote_addr)
         log_data.append("-")
@@ -11,18 +13,22 @@ def apache_logger(callback):
         log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
         log_data.append('"%s %s %s"' % (bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
         log_data.append(str(bottle.response.status_code))
-        log_data.append("-") #TODO: Fix content-length
-        log_data.append(bottle.request.get_header("Referer", "-"))
-        log_data.append(bottle.request.get_header("User-Agent", "-"))
+        log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
+        log_data.append("\"%s\"" % bottle.request.get_header("Referer", "-"))
+        log_data.append("\"%s\"" % bottle.request.get_header("User-Agent", "-"))
 
         with open("server.log", "a+") as log_file:
             log_file.write(" ".join(log_data) + "\n")
+
+        return body
 
     return wrapper
 
 def common_logger(callback):
     """Logs in the common log format"""
     def wrapper(*args, **kwargs):
+        body = callback(*args, **kwargs)
+
         log_data = []
         log_data.append(bottle.request.remote_addr)
         log_data.append("-")
@@ -30,9 +36,11 @@ def common_logger(callback):
         log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
         log_data.append('"%s %s %s"' % (bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
         log_data.append(str(bottle.response.status_code))
-        log_data.append("-") #TODO: Fix content-length
+        log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
 
         with open("server.log", "a+") as log_file:
             log_file.write(" ".join(log_data) + "\n")
+
+        return body
 
     return wrapper
