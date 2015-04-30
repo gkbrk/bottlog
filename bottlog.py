80fe02d1e231097f35899ad255e34acfe18c2069
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-04-30 18:19:48 +0300

    Fixed missing arguments bug

--- bottlog.py
+++ bottlog.py
@@ -8,28 +8,30 @@ class apache_logger:
         self.logfile = logfile
 
     def apply(self, callback, route):
-        body = callback(*args, **kwargs)
+        def wrapper(*args, **kwargs):
+            body = callback(*args, **kwargs)
 
-        log_data = []
-        log_data.append(bottle.request.remote_addr)
-        log_data.append("-")
-        log_data.append("-")
-        log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
-        log_data.append("\"%s %s %s\"".format(bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
-        log_data.append(str(bottle.response.status_code))
-        log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
+            log_data = []
+            log_data.append(bottle.request.remote_addr)
+            log_data.append("-")
+            log_data.append("-")
+            log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
+            log_data.append("\"%s %s %s\"".format(bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
+            log_data.append(str(bottle.response.status_code))
+            log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
 
-        referer = bottle.request.get_header("Referer", "-")
-        if referer != "-":
-            referer = "\"%s\"".format(referer)
-        log_data.append(referer)
+            referer = bottle.request.get_header("Referer", "-")
+            if referer != "-":
+                referer = "\"%s\"".format(referer)
+            log_data.append(referer)
 
-        user_agent = bottle.request.get_header("User-Agent", "-")
-        if user_agent != "-":
-            user_agent = "\"%s\"".format(user_agent)
-        log_data.append(user_agent)
+            user_agent = bottle.request.get_header("User-Agent", "-")
+            if user_agent != "-":
+                user_agent = "\"%s\"".format(user_agent)
+            log_data.append(user_agent)
 
-        with open(self.logfile, "a+") as log_file:
-            log_file.write("{}\n".format(" ".join(log_data)))
+            with open(self.logfile, "a+") as log_file:
+                log_file.write("{}\n".format(" ".join(log_data)))
 
-        return body
+            return body
+        return wrapper
