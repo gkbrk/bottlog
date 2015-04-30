1fab541ffc571b6b800545658d346d7ec82c9474
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-04-30 18:21:43 +0300

    Fixed log format to match apache

--- bottlog.py
+++ bottlog.py
@@ -16,18 +16,18 @@ class apache_logger:
             log_data.append("-")
             log_data.append("-")
             log_data.append(time.strftime("[%d/%b/%Y:%H:%M:%S %z]"))
-            log_data.append("\"%s %s %s\"".format(bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
+            log_data.append("\"{} {} {}\"".format(bottle.request.method, bottle.request.path, bottle.request.environ.get("SERVER_PROTOCOL", "")))
             log_data.append(str(bottle.response.status_code))
             log_data.append(str(len(body) if not isinstance(body, bottle.HTTPResponse) else 0))
 
             referer = bottle.request.get_header("Referer", "-")
             if referer != "-":
-                referer = "\"%s\"".format(referer)
+                referer = "\"{}\"".format(referer)
             log_data.append(referer)
 
             user_agent = bottle.request.get_header("User-Agent", "-")
             if user_agent != "-":
-                user_agent = "\"%s\"".format(user_agent)
+                user_agent = "\"{}\"".format(user_agent)
             log_data.append(user_agent)
 
             with open(self.logfile, "a+") as log_file:
